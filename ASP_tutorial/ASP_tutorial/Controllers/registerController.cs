7b7ce049b1fdd90891b5e7f55d4307cf4373a111
onur akyıldız <onurakyildi41@gmail.com>
2023-07-29 01:46:19 +0300

    .net tutorial

--- ASP_tutorial/ASP_tutorial/Controllers/registerController.cs
+++ ASP_tutorial/ASP_tutorial/Controllers/registerController.cs
@@ -1,17 +1,32 @@
-﻿using System;
+﻿using ASP_tutorial.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
+using System.Text;
 using System.Web;
+using System.Web.Hosting;
 using System.Web.Mvc;
 
 namespace ASP_tutorial.Controllers
 {
     public class registerController : Controller
     {
-        // GET: register
+        DB_tutorialEntities db = new DB_tutorialEntities();
+
         public ActionResult Index()
         {
             return View();
+        }
+        
+        public JsonResult saveData(TBL_users model)
+        {
+            model.isExists = false;
+            model.createdAt = DateTime.Now;
+            db.TBL_users.Add(model);
+            db.SaveChanges();
+            return Json("Kayıt Başarılı", JsonRequestBehavior.AllowGet);
+
         }
     }
 }
