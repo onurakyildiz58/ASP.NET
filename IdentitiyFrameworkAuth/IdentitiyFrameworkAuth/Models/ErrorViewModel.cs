7573988f78ddde7738f6f7c2fb9856f2ce6fabde
onur akyıldız <onurakyildi41@gmail.com>
2023-08-08 10:07:09 +0300

    identity framework

---
+++ IdentitiyFrameworkAuth/IdentitiyFrameworkAuth/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IdentitiyFrameworkAuth.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
